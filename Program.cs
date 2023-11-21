da4eb3ff53f8438b641f45369c7c28611dbd6d91
Yelayou Gebremeskel <yelayou@gmail.com>
2023-11-21 08:52:22 -0500

    DB connected with Docker as localhost

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace EfDemo
 {
@@ -7,7 +8,30 @@ namespace EfDemo
         static void Main(string[] args)
         {
             // Change the following connection string value as required to connect to your SQL Server instance and DB
-            var connectionString = "Server=(local);Database=EfDemo;Trusted_Connection=True;";
+            //var connectionString = "Server=(localhost:1433);Database=EfDemo;Trusted_Connection=True;";
+            var connectionString = "Data Source=localhost,1433;Initial Catalog=EfDemo;Database=EfDemo; User=sa;Password=P@$$w0rd;TrustServerCertificate=True";
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Open the database connection
+                    connection.Open();
+
+                    // TODO: Add your database operations here
+
+                    Console.WriteLine("Connected to the database successfully.");
+
+                    // Close the connection when done
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
             Console.WriteLine("---------- Program started ----------");
             using (var context = new EfDemoDbContext(connectionString))
             {
